2cbd125626d68ace7e9298c0529d9a691a6982a4
Sydney <github@shonx.net>
2024-03-06 23:01:55 -0500

    Initial.

---
+++ AzureHandler.cs
@@ -0,0 +1,77 @@
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+
+public class AzureHandler
+{
+    private BlobServiceClient bsc;
+    private BlobContainerClient bcc;
+
+    private BlobClient bc;
+
+    private readonly string FILE_NAME = "emails.json";
+    public AzureHandler()
+    {
+        bsc = new BlobServiceClient(
+                new Uri("https://weathertrackerb951.blob.core.windows.net"),
+                new DefaultAzureCredential());
+        bcc = bsc.GetBlobContainerClient("emails");
+        bc = bcc.GetBlobClient(FILE_NAME);
+    }
+
+    public List<string> GetEmails()
+    {
+        return GetEmails0().Result;
+
+    }
+
+    public void AddEmail(Email email)
+    {
+        var emails = GetEmails();
+        emails.Add(email.email);
+        SaveEmails(emails);
+    }
+
+    public bool DeleteEmail(Email email)
+    {
+        var emails = GetEmails();
+        bool removed = emails.Remove(email.email);
+        if(removed)
+            SaveEmails(emails);
+        return removed;
+    }
+
+
+    private async Task<List<string>> GetEmails0()
+    {
+        string localFilePath = GetLocalFilePath();
+
+        await bc.DownloadToAsync(localFilePath);
+
+        string json = await File.ReadAllTextAsync(localFilePath);
+
+        return JsonConvert.DeserializeObject<List<string>>(json);
+    }
+
+    private async void SaveEmails(List<string> emails)
+    {
+        string localFilePath = GetLocalFilePath();
+
+        await File.WriteAllTextAsync(localFilePath, JsonConvert.SerializeObject(emails));
+
+        await bc.UploadAsync(localFilePath, true);
+    }
+
+    private string GetLocalFilePath()
+    {
+        string localPath = "temp";
+        Directory.CreateDirectory(localPath);
+        string fileName = Guid.NewGuid().ToString() + "_" + FILE_NAME;
+        return Path.Combine(localPath, fileName);
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+using System.Net;
+using System.Text;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        WebApplication app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        AzureHandler handler = new AzureHandler();
+
+        app.UseHttpsRedirection();
+
+        app.MapPut("/emails/add/", async context =>
+        {
+            if (context.Request.ContentType != "application/json")
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return;
+            }
+            string json = await ReadLines(context.Request.Body);
+            Email email;
+            try
+            {
+                email = JsonConvert.DeserializeObject<Email>(json);
+            }
+            catch (Exception e)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return;
+            }
+            if (email is null || email.email == null)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return;
+            }
+            handler.AddEmail(email);
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+        })
+        .WithName("RegisterEmail")
+        .WithOpenApi();
+
+        app.Run();
+    }
+
+    private static async Task<string> ReadLines(Stream stream)
+    {
+        using (var reader = new StreamReader(stream, Encoding.UTF8))
+        {
+            string result = "";
+            string line;
+            while (true)
+            {
+                line = await reader.ReadLineAsync();
+                if (line is null)
+                    return result;
+                result += line;
+            }
+        }
+    }
+}
+
